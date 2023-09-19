4465f1fc5396ffbe81e046984e0d4e671b574f3a
jose david <81387297+josedavid1996@users.noreply.github.com>
2023-09-19 06:12:51 -0500

    Se completo el menu mobile

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,12 @@ const $document = document
 const $menuMobile = $document.querySelector(".navigation-mobile")
 const $menu = $document.querySelector("#menu")
 
-
-$document.addEventListener("click", (e) => {
-  console.log(e.target === $menu)
-  if (e.target === $menu) {
-    $menuMobile.style.right = "0px"
-  }
-  if (e.target === $menuMobile) {
-    $menuMobile.style.right = "100%"
-  }
+$menu.addEventListener("click", () => {
+  $menuMobile.style.right = "0px"
 })
+
+$menuMobile.addEventListener("click", () => {
+  $menuMobile.style.right = "100%"
+})
+
+
